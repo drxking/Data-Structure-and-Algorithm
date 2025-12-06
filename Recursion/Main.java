3f9efbfefb67587c171b94707e9f7fa956a3dff4
drxking <drxking456@gmail.com>
2025-12-06 17:47:33 +0545

    GCD

--- Recursion/Main.java
+++ Recursion/Main.java
@@ -2,10 +2,11 @@ package Recursion;
 
 public class Main {
     public static void main(String[] args) {
-        Recursion r1 = new Recursion();
-        for(int i = 0;i<=10;i++){
-            System.out.println(r1.fabonacci(i));
+        // Recursion r1 = new Recursion();
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(Recursion.fabonacci(i));
         }
-        System.out.println("Factorial of 10 is " + r1.factorial(10));
+        System.out.println("Factorial of 10 is " + Recursion.factorial(10));
+        System.out.println(Recursion.gcd(18, 48));
     }
 }
--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -1,15 +1,26 @@
 package Recursion;
 
 public class Recursion {
-    public int fabonacci(int num){
-        if(num == 0) return 0;
-        if(num == 1) return 1;
-        return this.fabonacci(num-1) + this.fabonacci(num-2);
+    public static int fabonacci(int num) {
+        if (num == 0)
+            return 0;
+        if (num == 1)
+            return 1;
+        return fabonacci(num - 1) + fabonacci(num - 2);
     }
 
-    public int factorial(int num){
-        if(num == 0) return 0;
-        if(num == 1) return 1;
-        return num * this.factorial(num-1);
+    public static int factorial(int num) {
+        if (num == 0)
+            return 0;
+        if (num == 1)
+            return 1;
+        return num * factorial(num - 1);
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
     }
 }
