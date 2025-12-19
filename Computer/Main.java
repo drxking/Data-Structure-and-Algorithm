1b02ebb8b5bb0729349fbcc7c0835fb94d6275e8
drxking <drxking456@gmail.com>
2025-12-20 00:10:13 +0545

    Teaching

--- Computer/Main.java
+++ Computer/Main.java
@@ -1,7 +1,6 @@
 package Computer;
 public class Main {
     public static void main(String[] args) {
-        Computer hp = new Computer("HP",16, "en-us", "Black");
 
         Computer dell = new Computer("Dell",34, "en-uk", "Green");
 
